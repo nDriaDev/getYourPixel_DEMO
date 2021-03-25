2f5b55566765f10f5cdef6475b0cb5809224840f
4ndr3w_c0 <andreacosentino.work@gmail.com>
2021-03-25 01:49:31 +0100

    Fix navbar dimension

--- src/components/mainLayout/header/header.jsx
+++ src/components/mainLayout/header/header.jsx
@@ -4,10 +4,10 @@ import NavbarCustom from './../../navbar/navbar';
 
 const Header = React.memo((props) => {
 
-  return(
+  return (
     <>
-      <div className="sticky-top background-fusion">
-        <NavbarCustom enableSpinner={props.enableSpinner} disableSpinner={props.disableSpinner} setAuth={props.setAuth} isAuth={props.isAuth} isAuthBasic={props.isAuthBasic}/>
+      <div className="sticky-top background-fusion" style={{ height: 'fit-content' }}>
+        <NavbarCustom enableSpinner={props.enableSpinner} disableSpinner={props.disableSpinner} setAuth={props.setAuth} isAuth={props.isAuth} isAuthBasic={props.isAuthBasic} />
       </div>
     </>
   )
