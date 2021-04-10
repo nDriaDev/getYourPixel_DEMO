30f5566d616bc14de6eef88d3aa96d96a943bff9
4ndr3w_c0 <andreacosentino.work@gmail.com>
2021-04-10 13:03:11 +0200

    Fix

--- src/components/buy/buyManager.jsx
+++ src/components/buy/buyManager.jsx
@@ -9,9 +9,7 @@ export const BuyManager = React.memo(({ component: Component, ...rest }) => {
         const script = document.createElement('script');
         script.src = Const.PAYPAL.sdk + (process.env.REACT_APP_PAYPAL_CLIENT_ID ?
             process.env.REACT_APP_PAYPAL_CLIENT_ID :
-            // '<your clientID here>'
-            // 'Adr9u1cFoRoxvGD60HKXhtnS3J8CmZiGdwTcgGuVVck2nS0VdRaWw45RtVe1T7TLxmesjJWMF0Q1mn5p'
-            'AelN2h6SpD6n31DN4Ubhf6cCLOnRqCybnoegCMaVkjRDgiel4Quz9kDzFPWCWWFht6jm_pu_vG4wcT0v'
+            '<your clientID here>'
         ) + '&currency=EUR';
 
         script.onload = () => {
